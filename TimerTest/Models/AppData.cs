d67449f074e5df5edede56a91dad20ff07a1df32
vankadulapa <vankadulapa@DESKTOP-CBMOSED>
2023-06-15 06:29:41 +0300

    Add project files.

---
+++ TimerTest/Models/AppData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerTest.Models
+{
+    static public class AppData
+    {
+        static public OOO_MalyshEntities db = new OOO_MalyshEntities();
+        static public int CurrentAccessLevel = 0;
+    }
+}
